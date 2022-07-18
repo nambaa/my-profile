dbf4bff950c9ddc5496e9b760df531a52a42fe57
mstars_lab3_17 <mstars_lab3_17@mstars-lab3-17s-iMac.local>
2022-07-18 12:49:57 +0800

    index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,8 +4,28 @@ require("dotenv").config();
 app.use(express.static("public"));
 const PORT = process.env.PORT;
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view options", { layout: false });
+app.get("/ejs", (req, res) => {
+  res.render("index");
+});
+app.get("/", function (req, res) {
+  res.render("index", { name: "naba" });
+});
+app.get("/404", function (req, res) {
+  res.render("404", { message: "oopsie!" });
+});
+
 app.get("/api", (req, res) => {
   res.send("hello world");
+});
+app.get("/js", function (req, res) {
+  var data = {
+    name: "naba",
+    hobbies: ["playing soccer", "playing e-sport", "cycling"],
+  };
+  res.render("js", { data: data });
 });
 
 app.listen(PORT);
